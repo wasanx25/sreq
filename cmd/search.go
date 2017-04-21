4abba2f484dd3e774a53512e820bbf59137bd8c2
wataru kikuchi <kikuchi@fms-l45.fujisan.local>
2017-04-21 13:35:20 +0900

    Add sample api call

--- cmd/search.go
+++ cmd/search.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -11,10 +13,25 @@ var searchCmd = &cobra.Command{
 	Short: "Call Api of Qiita",
 	Long:  "Call Api of Qiita",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("search called")
+		resp, err := http.Get("http://qiita.com/api/v2/items?page=1&per_page=2&query=ruby")
+		if err == nil {
+			puts(resp)
+		} else {
+			fmt.Println(err)
+		}
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(searchCmd)
+}
+
+func puts(resp *http.Response) {
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err == nil {
+		fmt.Println(string(b))
+	} else {
+		fmt.Println(err)
+	}
 }
