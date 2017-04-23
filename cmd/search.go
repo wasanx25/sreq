78febcb38708e5c304d67d10c12d32c845c82220
wataru0225 <wataru.kikuchi0225@gmail.com>
2017-04-23 20:50:45 +0900

    Add json parse method

--- cmd/search.go
+++ cmd/search.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,12 +9,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type Qiita struct {
+	Title string `json: "title"`
+	Url   string `json: "url"`
+}
+
 var searchCmd = &cobra.Command{
 	Use:   "search",
 	Short: "Call Api of Qiita",
 	Long:  "Call Api of Qiita",
 	Run: func(cmd *cobra.Command, args []string) {
-		resp, err := http.Get("http://qiita.com/api/v2/items?page=1&per_page=2&query=ruby")
+		resp, err := http.Get("http://qiita.com/api/v2/items?page=1&per_page=10&query=ruby")
 		if err == nil {
 			puts(resp)
 		} else {
@@ -30,7 +36,9 @@ func puts(resp *http.Response) {
 	defer resp.Body.Close()
 	b, err := ioutil.ReadAll(resp.Body)
 	if err == nil {
-		fmt.Println(string(b))
+		var content []Qiita
+		json.Unmarshal(b, &content)
+		fmt.Printf("%+v", content)
 	} else {
 		fmt.Println(err)
 	}
