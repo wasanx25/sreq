fefbf7ad8a1b25c81bbcc9fc36699f9bb9d6be2f
wataru0225 <wataru.kikuchi0225@gmail.com>
2017-05-08 18:25:03 +0900

    Modified history color

--- cmd/history.go
+++ cmd/history.go
@@ -16,9 +16,9 @@ var historyCmd = &cobra.Command{
 		var snippets snippet.Snippets
 		snippets.Load()
 		for _, snip := range snippets.Snippets {
-			fmt.Println(color.YellowString(snip.Url))
-			fmt.Println(color.GreenString(snip.Title))
-			fmt.Println(color.GreenString(snip.SearchKeyword))
+			fmt.Println(color.YellowString("url: " + snip.Url))
+			fmt.Println(color.GreenString("title: " + snip.Title))
+			fmt.Println(color.CyanString("keyword: " + snip.SearchKeyword))
 			fmt.Println(color.WhiteString("-------------------------------"))
 		}
 	},
