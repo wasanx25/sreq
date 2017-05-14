c9cef6c3da77459342008b169877596d4d816a71
wataru0225 <wataru.kikuchi0225@gmail.com>
2017-05-15 07:59:02 +0900

    Delete mkdir code

--- snippet/snippet.go
+++ snippet/snippet.go
@@ -3,7 +3,6 @@ package snippet
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/BurntSushi/toml"
 )
@@ -19,10 +18,6 @@ type SnippetInfo struct {
 }
 
 func (snippets *Snippets) Load(fileName string) error {
-	dir := filepath.Join(os.Getenv("HOME"), ".config", "sreq")
-	if err := os.MkdirAll(dir, 0700); err != nil {
-		fmt.Errorf("cannot create directory: %v", err)
-	}
 	snippetFile := fileName
 	if _, err := toml.DecodeFile(snippetFile, snippets); err != nil {
 		return fmt.Errorf("Failed to load snippet file. %v", err)
