81fa6854b62a2a66cd470edc93eac8e9c661fd46
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-25 00:53:20 +0900

    Add search exec method

--- search/new_search.go
+++ search/new_search.go
@@ -3,12 +3,23 @@ package search
 import (
 	"net/url"
 	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/wasanx25/sreq/config"
 )
 
 type search struct {
 	Keyword    string
 	Pagenation int
 	Sort       string
+	Contents   []*Content
+}
+
+// Content is structure that scraping content from Qiita
+type Content struct {
+	ID    string
+	Title string
+	Desc  string
 }
 
 func New(keyword string, sort string) *search {
@@ -36,4 +47,29 @@ func (s *search) GetURL() string {
 
 func (s *search) NextPage() {
 	s.Pagenation++
+}
+
+func (s *search) Exec() ([]*Content, error) {
+	url := config.GetPageURL(s.Keyword, s.Sort, strconv.Itoa(s.Pagenation))
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		return nil, err
+	}
+	doc.Find(".searchResult").Each(s.getAttr)
+
+	return s.Contents, nil
+}
+
+func (s *search) getAttr(_ int, q *goquery.Selection) {
+	itemID, _ := q.Attr("data-uuid")
+	title := q.Find(".searchResult_itemTitle a").Text()
+	desc := q.Find(".searchResult_snippet").Text()
+
+	content := &Content{
+		ID:    itemID,
+		Title: title,
+		Desc:  desc,
+	}
+
+	s.Contents = append(s.Contents, content)
 }
