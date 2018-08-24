5c95bc4528b2bc0dcd9bb1609e28ee0fa0d7008c
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-25 00:50:53 +0900

    Add search get url method

--- search/new_search.go
+++ search/new_search.go
@@ -1,5 +1,10 @@
 package search
 
+import (
+	"net/url"
+	"strconv"
+)
+
 type search struct {
 	Keyword    string
 	Pagenation int
@@ -12,4 +17,19 @@ func New(keyword string, sort string) *search {
 		Pagenation: 0,
 		Sort:       sort,
 	}
+}
+
+func (s *search) GetURL() string {
+	q := url.Values{}
+	q.Set("pagenetion", strconv.Itoa(s.Pagenation))
+	q.Set("q", s.Keyword)
+	q.Set("sort", s.Sort)
+	u := url.URL{
+		Scheme:   "https",
+		Host:     "qiita.com",
+		Path:     "search",
+		RawQuery: q.Encode(),
+	}
+
+	return u.String()
 }
