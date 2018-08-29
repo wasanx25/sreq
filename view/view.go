4c532bb2851152f6cd56ac42c7e12810b9e83e8f
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-29 23:46:04 +0900

    Add cmd run exec in view pkg

--- view/view.go
+++ view/view.go
@@ -1,1 +1,36 @@
 package view
+
+import (
+	"os"
+	"os/exec"
+)
+
+type Command string
+
+type View struct {
+	Cmd    Command
+	Option []string
+	File   string
+}
+
+const (
+	LESS = "less"
+	LYNX = "lynx"
+)
+
+func New(cmd Command, file string, option ...string) *View {
+	return &View{
+		Cmd:    cmd,
+		File:   file,
+		Option: option,
+	}
+}
+
+func (v *View) Exec() {
+	var args []string
+	args = append(v.Option, v.File)
+	cmd := exec.Command(string(v.Cmd), args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
