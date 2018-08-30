9e9100124d41999d6999d244fd8463f3b5ee2ea0
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-30 23:15:49 +0900

    Add get content method

--- control/control.go
+++ control/control.go
@@ -1,1 +1,20 @@
 package control
+
+import (
+	"fmt"
+	"net/url"
+	"path/filepath"
+	"strconv"
+)
+
+type Control struct {
+}
+
+func (c *Control) GetURL(id string) string {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "qiita.com",
+		Path:   filepath.Join("api", "v2", "items", id),
+	}
+	return u.String()
+}
--- control/control_test.go
+++ control/control_test.go
@@ -1,1 +1,18 @@
 package control_test
+
+import (
+	"testing"
+
+	"github.com/wasanx25/sreq/control"
+)
+
+func TestGetURL(t *testing.T) {
+	c := &control.Control{}
+
+	actual := c.GetURL("test")
+	expected := "https://qiita.com/api/v2/items/test"
+
+	if actual != expected {
+		t.Errorf("expected=%q, got=%q", expected, actual)
+	}
+}
