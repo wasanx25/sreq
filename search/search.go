fd3572dffc973a4be67731e4f42b23be48241a3c
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-31 01:10:07 +0900

    Move new_search -> search

--- search/search.go
+++ search/search.go
@@ -1,124 +1,91 @@
 package search
 
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"io/ioutil"
-// 	"net/http"
-// 	"os"
-// 	"os/exec"
-// 	"strconv"
-//
-// 	"github.com/PuerkitoBio/goquery"
-// 	"github.com/fatih/color"
-// 	"github.com/wasanx25/sreq/config"
-// 	"github.com/wasanx25/sreq/history"
-// )
-//
-// // Content is structure that scraping content from Qiita
-// type Content struct {
-// 	ID    string
-// 	Title string
-// 	Desc  string
-// }
-//
-// func search(argument string, pagenation int, sort string) ([]*Content, error) {
-// 	doc, err := goquery.NewDocument(config.GetPageURL(argument, sort, strconv.Itoa(pagenation)))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	var contents []*Content
-//
-// 	doc.Find(".searchResult").Each(func(_ int, s *goquery.Selection) {
-// 		itemID, _ := s.Attr("data-uuid")
-// 		title := s.Find(".searchResult_itemTitle a").Text()
-// 		desc := s.Find(".searchResult_snippet").Text()
-//
-// 		content := &Content{
-// 			ID:    itemID,
-// 			Title: title,
-// 			Desc:  desc,
-// 		}
-//
-// 		contents = append(contents, content)
-// 	})
-//
-// 	return contents, nil
-// }
-//
-// func viewList(contents []*Content) {
-// 	for num, content := range contents {
-// 		fmt.Print(color.YellowString(strconv.Itoa(num) + " -> "))
-// 		fmt.Println(content.Title)
-// 		fmt.Println(color.GreenString(content.Desc))
-// 		fmt.Print("\n")
-// 	}
-// 	if len(contents) == 10 {
-// 		fmt.Println(color.YellowString("n -> ") + "next page")
-// 	}
-// 	fmt.Print("SELECT > ")
-// }
-//
-// func scan(contents []*Content, argument string, lynx bool) bool {
-// 	var num string
-// 	if _, err := fmt.Scanf("%s", &num); err != nil {
-// 		fmt.Println(err)
-// 	}
-//
-// 	if num == "n" {
-// 		return false
-// 	}
-//
-// 	index, _ := strconv.Atoi(num)
-// 	target := contents[index]
-//
-// 	resp, err := http.Get(config.GetAPIURL(target.ID))
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	defer resp.Body.Close()
-//
-// 	b, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	var qiita *config.Qiita
-// 	json.Unmarshal(b, &qiita)
-//
-// 	writeHistory(qiita, argument)
-//
-// 	if lynx {
-// 		openFile(qiita.HTML, "/tmp/sreq.html", "lynx", "-display_charset=utf-8", "-assume_charset=utf-8")
-// 		return true
-// 	}
-//
-// 	openFile(qiita.Markdown, "/tmp/sreq.txt", "less")
-// 	return true
-// }
-//
-// func openFile(body string, file string, cmdName ...string) {
-// 	text := []byte(body)
-// 	ioutil.WriteFile(file, text, os.ModePerm)
-// 	cmdName = append(cmdName, file)
-// 	cmd := exec.Command(cmdName[0], cmdName[1:]...)
-// 	cmd.Stdin = os.Stdin
-// 	cmd.Stdout = os.Stdout
-// 	cmd.Run()
-// }
-//
-// func writeHistory(content *config.Qiita, argument string) {
-// 	var snippets history.Snippets
-// 	snippets.Load()
-// 	url := content.URL
-// 	newSnippet := history.Snippet{
-// 		SearchKeyword: argument,
-// 		URL:           url,
-// 		Title:         content.Title,
-// 	}
-// 	snippets.Snippets = append(snippets.Snippets, newSnippet)
-// 	if err := snippets.Save(); err != nil {
-// 		fmt.Printf("Failed. %v", err)
-// 		os.Exit(2)
-// 	}
-// }
+import (
+	"bytes"
+	"net/url"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/fatih/color"
+)
+
+type search struct {
+	Keyword    string
+	Pagenation int
+	Sort       string
+	Contents   []*Content
+}
+
+// Content is structure that scraping content from Qiita
+type Content struct {
+	ID    string
+	Title string
+	Desc  string
+}
+
+func New(keyword string, sort string) *search {
+	return &search{
+		Keyword:    keyword,
+		Pagenation: 0,
+		Sort:       sort,
+	}
+}
+
+func (s *search) GetURL() string {
+	q := url.Values{}
+	q.Set("page", strconv.Itoa(s.Pagenation))
+	q.Set("q", s.Keyword)
+	q.Set("sort", s.Sort)
+	u := url.URL{
+		Scheme:   "https",
+		Host:     "qiita.com",
+		Path:     "search",
+		RawQuery: q.Encode(),
+	}
+
+	return u.String()
+}
+
+func (s *search) NextPage() {
+	s.Pagenation++
+}
+
+func (s *search) Exec(page string) ([]*Content, error) {
+	doc, err := goquery.NewDocument(page)
+	if err != nil {
+		return nil, err
+	}
+	doc.Find(".searchResult").Each(s.getAttr)
+
+	return s.Contents, nil
+}
+
+func (s *search) ContentString() string {
+	var out bytes.Buffer
+
+	for n, c := range s.Contents {
+		out.WriteString(color.YellowString(strconv.Itoa(n) + " -> "))
+		out.WriteString(c.Title + "\n")
+		out.WriteString(color.GreenString(c.Desc) + "\n\n")
+	}
+
+	if len(s.Contents) == 10 {
+		out.WriteString(color.YellowString("n -> ") + "next page\n")
+	}
+
+	return out.String()
+}
+
+func (s *search) getAttr(_ int, q *goquery.Selection) {
+	itemID, _ := q.Attr("data-uuid")
+	title := q.Find(".searchResult_itemTitle a").Text()
+	desc := q.Find(".searchResult_snippet").Text()
+
+	content := &Content{
+		ID:    itemID,
+		Title: title,
+		Desc:  desc,
+	}
+
+	s.Contents = append(s.Contents, content)
+}
